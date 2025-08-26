663466c2bd1b2f2e0653281ad90509f2a5a86d3a
Pola Srinitha <polasrinitha@gmail.com>
2025-08-26 08:31:09 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@ void montecarlo_sequential() {
     std::cout << "[Sequential Monte Carlo placeholder]" << std::endl;
 }
 
+void montecarlo_mpi() {
+    std::cout << "[MPI Monte Carlo placeholder]" << std::endl;
+}
+
 void montecarlo_openmp() {
     std::cout << "[OpenMP Monte Carlo placeholder]" << std::endl;
     #pragma omp parallel
@@ -13,14 +17,6 @@ void montecarlo_openmp() {
         std::cout << "Thread " << omp_get_thread_num()
                   << " of " << omp_get_num_threads() << std::endl;
     }
-}
-
-void montecarlo_mpi() {
-    std::cout << "[MPI Monte Carlo placeholder]" << std::endl;
-}
-
-void montecarlo_cuda() {
-    std::cout << "[CUDA Monte Carlo placeholder]" << std::endl;
 }
 
 // ---------- Main driver ----------
@@ -44,14 +40,11 @@ int main() {
     std::cout << "\n[1] Sequential Monte Carlo..." << std::endl;
     montecarlo_sequential();
 
-    std::cout << "\n[2] OpenMP parallel version..." << std::endl;
-    montecarlo_openmp();
-
-    std::cout << "\n[3] MPI distributed version..." << std::endl;
+    std::cout << "\n[2] MPI distributed version..." << std::endl;
     montecarlo_mpi();
 
-    std::cout << "\n[4] CUDA GPU version..." << std::endl;
-    montecarlo_cuda();
+    std::cout << "\n[3] OpenMP parallel version..." << std::endl;
+    montecarlo_openmp();
 
     std::cout << "\nSimulation complete" << std::endl;
     return 0;
